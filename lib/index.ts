10cf5dbd87f4c35ceb5e2dc78f52de7eede049eb
Tim Branyen <tbranyen@netflix.com>
2018-11-06 21:24:55 -0800

    Fully resolve preset plugin paths

--- lib/index.ts
+++ lib/index.ts
@@ -1,15 +1,17 @@
 import { declare } from '@babel/helper-plugin-utils';
 
+const { resolve } = require;
+
 export default declare((api, options) => {
   api.assertVersion(7);
 
   return {
     plugins: [
-      'babel-plugin-transform-inline-environment-variables',
-      'babel-plugin-minify-dead-code-elimination',
-      'babel-plugin-transform-commonjs',
-      'babel-plugin-resolve-imports-for-browser',
-      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      resolve('babel-plugin-transform-inline-environment-variables'),
+      resolve('babel-plugin-minify-dead-code-elimination'),
+      resolve('babel-plugin-transform-commonjs'),
+      resolve('babel-plugin-resolve-imports-for-browser'),
+      [resolve('@babel/plugin-proposal-class-properties'), { loose: true }],
     ],
   };
 });
